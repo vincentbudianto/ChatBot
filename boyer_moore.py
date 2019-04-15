fc741baac410ff06533086c7f8a70fd3b0face58
AgentNine <juniardiakbar@gmail.com>
2019-04-15 22:48:11 +0700

    remove stop words

--- boyer_moore.py
+++ boyer_moore.py
@@ -1,18 +1,17 @@
-def file():
-	f = open("pertanyaan.txt", "r")
+from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
+
+def readFile(str):
+	f = open(str, "r")
 	if f.mode == "r":
 		contents = f.read()
 		return contents
 
 def Boyer_Moore (T,P) :
-
 	match = False
-	
 	m = len(P)
 	n = len(T)
 	i = m-1
 	j = m-1
-	id = 0
 	
 	while (i <= n-1 and not match) :
 		if (T[i].lower() == P[j].lower()):
@@ -36,18 +35,49 @@ def Boyer_Moore (T,P) :
 		return -1
 
 if __name__ == "__main__":
-	T = file()
+	T = readFile("pertanyaan.txt")
 	print("Masukkan Pattern: ")
 	P = str(input())
-
-	id = Boyer_Moore(T,P)
-
+ 
+	factory = StopWordRemoverFactory()
+	stopword = factory.create_stop_word_remover()
+	stop = stopword.remove(P)
+	stop = stop.replace('?','')
+	id = Boyer_Moore(T,stop)
+	
+	idx = 0
 	if (id != -1):
-		idx = 0
 		for i in range(id):
 			if (T[i] == '\n'):
 				idx = idx+1
 
-		print(idx)
+		str = ''
+		while (T[id] != '\n'):
+			id = id-1
+		
+		while (T[id+1] != '\n'):
+			str += T[id+1]
+			id = id+1
+
+		if (str.lower() != stop.lower()):
+			idx = -1
 	else:
-		print(id)
+		idx = -1
+
+	if (idx != -1):
+		J = readFile("jawaban.txt")
+		n = 0
+		ans = ''
+		i = 0
+		while (n != idx):
+			if (J[i] == '\n'):
+				n = n+1
+			i = i+1
+
+		while (J[i] != '\n'):
+			ans += J[i]
+			i = i+1
+
+		print(ans)
+		# print(idx)
+		
