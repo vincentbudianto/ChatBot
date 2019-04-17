8589695aa86cffd2299b2201eeb65809b9fb1aa8
Juniardi Akbar <juniardiakbar@gmail.com>
2019-04-17 20:55:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import sys
 from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 from boyer_moore import *
 from kmp import *
 from regex import *
+from synonim import *
 
 def readFile(str):
 	f = open(str, "r")
@@ -10,66 +12,61 @@ def readFile(str):
 		return contents
 
 if __name__ == "__main__":
-	T = readFile("pertanyaan.txt")
-	Z1 = T.split('\n')
-	Z2 = []
-	Z3 = []
-	
-	for i in range(len(Z1)):
-		Z2.append(Z1[i].split('. '))
+    factory = StopWordRemoverFactory()
+    stopword = factory.create_stop_word_remover()
+    
+    T = readFile("pertanyaan.txt")
+    Z1 = T.split('\n')
+    Z2 = []
+    Z3 = []
 
-	for j in range(len(Z2)):
-		Z3.append(Z2[j][1].split('? '))
-	
-	Q = ''
-	
-	for k in range(len(Z3)):
-		Q += Z3[k][0]
-		
-		if (k != len(Z3)):
-			Q += '\n'
-	
-	print("Masukkan Pattern: ")
-	P = str(input())
+    for i in range(len(Z1)):
+        Z2.append(Z1[i].split('. '))
 
-	factory = StopWordRemoverFactory()
-	stopword = factory.create_stop_word_remover()
-	stop = stopword.remove(P)
-	stop = stop.replace('?','')
-	pil = 0
-	
-	while (pil != 4):
-		print("Pilih Metode Pencarian")
-		print("1. Boyer Moore")
-		print("2. KMP")
-		print("3. Regex")
-		pil = int(input())
+    for j in range(len(Z2)):
+        Z3.append(Z2[j][1].split('? '))
 
-		if (pil == 1):
-			id = Boyer_Moore(Q, stop)
-		elif (pil == 2):
-			id = KMP(Q, stop)
-		elif (pil == 3):
-			id = regex(Q, stop)
-			
-		print(id)
+    Q = ''	
+    for k in range(len(Z3)):
+        temp = changeSinonim(Z3[k][0])
+        kata = len(temp.split(' '))
+        for i in range(kata):
+            temp = stopword.remove(temp)
+        Q += temp
+        
+        if (k != len(Z3)):
+            Q += '\n'
 
-		idx = 0
-		if (id != -1) and ((pil == 1) or (pil == 2)):
-			for i in range(id):
-				if (Q[i] == '\n'):
-					idx = idx+1
+    P = sys.argv[1]
+    P = P.replace('?','')
+    word = changeSinonim(P)
+    kata = len(word.split(' '))
+    for i in range(kata):
+        word = stopword.remove(word)
 
-			str = ''
-			while (Q[id] != '\n'):
-				id = id-1
-			
-			while (Q[id+1] != '\n'):
-				str += Q[id+1]
-				id = id+1
-		else:
-			idx = -1
-		
-		print(idx)
+    pil = int(sys.argv[2])
+    if (pil == 1):
+        id = Boyer_Moore(Q,word)
+    elif (pil == 2):
+        id = KMP(Q,word)
 
-		print(Z3[idx][1])
+    idx = 0
+    if (id != -1):
+        for i in range(id):
+            if (Q[i] == '\n'):
+                idx = idx+1
+
+        str = ''
+        while (Q[id] != '\n'):
+            id = id-1
+        
+        while (Q[id+1] != '\n'):
+            str += Q[id+1]
+            id = id+1
+    else:
+        idx = -1
+
+    if (idx != -1):
+        print(Z3[idx][1])
+    else:
+        print("Saya tidak mengerti maksud Anda :(")
