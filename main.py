61fd5f80d60d32ae1f1c00484c2d9750790fb329
vincent VB <vincent.budianto@yahoo.co.id>
2019-04-18 11:13:16 +0700

    fix regex

--- main.py
+++ main.py
@@ -12,61 +12,72 @@ def readFile(str):
 		return contents
 
 if __name__ == "__main__":
-    factory = StopWordRemoverFactory()
-    stopword = factory.create_stop_word_remover()
-    
-    T = readFile("pertanyaan.txt")
-    Z1 = T.split('\n')
-    Z2 = []
-    Z3 = []
+	factory = StopWordRemoverFactory()
+	stopword = factory.create_stop_word_remover()
+	
+	T = readFile("pertanyaan.txt")
+	Z1 = T.split('\n')
+	Z2 = []
+	Z3 = []
+	Q = ''	
 
-    for i in range(len(Z1)):
-        Z2.append(Z1[i].split('. '))
+	for i in range(len(Z1)):
+		Z2.append(Z1[i].split('. '))
 
-    for j in range(len(Z2)):
-        Z3.append(Z2[j][1].split('? '))
+	for j in range(len(Z2)):
+		Z3.append(Z2[j][1].split('? '))
 
-    Q = ''	
-    for k in range(len(Z3)):
-        temp = changeSinonim(Z3[k][0])
-        kata = len(temp.split(' '))
-        for i in range(kata):
-            temp = stopword.remove(temp)
-        Q += temp
-        
-        if (k != len(Z3)):
-            Q += '\n'
+	for k in range(len(Z3)):
+		Z3[k][0] = stopword.remove(changeSinonim(Z3[k][0])).lower()
+		temp = changeSinonim(Z3[k][0])
+		kata = len(temp.split(' '))
 
-    P = sys.argv[1]
-    P = P.replace('?','')
-    word = changeSinonim(P)
-    kata = len(word.split(' '))
-    for i in range(kata):
-        word = stopword.remove(word)
+		for i in range(kata):
+			temp = stopword.remove(temp)
 
-    pil = int(sys.argv[2])
-    if (pil == 1):
-        id = Boyer_Moore(Q,word)
-    elif (pil == 2):
-        id = KMP(Q,word)
+		Q += temp
+		
+		if (k != len(Z3)):
+			Q += '\n'
+		
+	P = sys.argv[1]
+	P = P.replace('?','')
+	word = changeSinonim(P)
+	kata = len(word.split(' '))
 
-    idx = 0
-    if (id != -1):
-        for i in range(id):
-            if (Q[i] == '\n'):
-                idx = idx+1
+	for l in range(kata):
+		word = stopword.remove(word).lower()
 
-        str = ''
-        while (Q[id] != '\n'):
-            id = id-1
-        
-        while (Q[id+1] != '\n'):
-            str += Q[id+1]
-            id = id+1
-    else:
-        idx = -1
+	pil = int(sys.argv[2])
 
-    if (idx != -1):
-        print(Z3[idx][1])
-    else:
-        print("Saya tidak mengerti maksud Anda :(")
+	if (pil == 1):
+		id = Boyer_Moore(Q, word)
+	elif (pil == 2):
+		id = KMP(Q, word)
+	elif (pil == 3):
+		id = regex(Z3, word)
+
+	idx = 0
+
+	if ((id != -1) and ((pil == 1) or (pil == 2))):
+		for i in range(id):
+			if (Q[i] == '\n'):
+				idx += 1
+
+		str = ''
+		
+		while (Q[id] != '\n'):
+			id -= 1
+		
+		while (Q[id + 1] != '\n'):
+			str += Q[id + 1]
+			id = id + 1
+	elif ((id != -1) and (pil == 3)):
+		idx = id
+	else:
+		idx = -1
+
+	if (idx != -1):
+		print(Z3[idx][1])
+	else:
+		print("Saya tidak mengerti maksud Anda :(")
--- regex.py
+++ regex.py
@@ -3,34 +3,34 @@ import re
 def regex(T, P):
 	stext = P.split()
 	lengthP = len(stext)
-	lengthT = len(stext)
+	lengthT = len(T)
 	found = False
 	i = 0
-	rtext = ''
 
 	while ((i < lengthT) and not(found)):
 		j = 0
+		rtext = ''
 		valid = True
-
+		
 		while ((j < lengthP) and (valid)):
 			rtext += stext[j]
-
+			
 			if (j != (len(stext) - 1)):
 				rtext += ' '
 			
-			x = re.search(rtext, P)
+			x = re.search(rtext, T[i][0])
 			
 			if (x == None):
 				valid = False
-			
-			j += 1
+			else:
+				j += 1
 		
 		if (j == lengthP):
 			found = True
-			
-		i += 1
+		else:
+			i += 1
 	
 	if (found):
-		return x.span()[1]
+		return i
 	else:
 		return -1
