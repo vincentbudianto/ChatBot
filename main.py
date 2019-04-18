a0a41c795399eda5d00644a04fcec658cc154fa9
vincent VB <vincent.budianto@yahoo.co.id>
2019-04-18 20:22:05 +0700

    added suggestion

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ from regex import *
 from synonim import *
 
 def readFile(str):
-	f = open(str, "r")
-	if f.mode == "r":
+	f = open(str, 'r')
+	if f.mode == 'r':
 		contents = f.read()
 		return contents
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	factory = StopWordRemoverFactory()
 	stopword = factory.create_stop_word_remover()
 	
-	T = readFile("pertanyaan.txt")
+	T = readFile('pertanyaan.txt')
 	Z1 = T.split('\n')
 	Z2 = []
 	Z3 = []
@@ -23,7 +23,7 @@ if __name__ == "__main__":
 
 	for i in range(len(Z1)):
 		Z2.append(Z1[i].split('. '))
-
+		
 	for j in range(len(Z2)):
 		Z3.append(Z2[j][1].split('? '))
 
@@ -55,8 +55,10 @@ if __name__ == "__main__":
 	elif (pil == 2):
 		id = KMP(Q, word)
 	elif (pil == 3):
-		id = regex(Z3, word)
-
+		id, percent = regex(Z3, word)
+	
+	ids = sorted(range(len(percent)), key=lambda i: percent[i])[-3:]
+	
 	idx = 0
 
 	if ((id != -1) and ((pil == 1) or (pil == 2))):
@@ -73,11 +75,32 @@ if __name__ == "__main__":
 			str += Q[id + 1]
 			id = id + 1
 	elif ((id != -1) and (pil == 3)):
-		idx = id
+		if (percent[id] >= 0.9):
+			idx = id
+		else:
+			ok = False
+			print('Mungkin maksud anda adalah:')
+			print('1. ' + Z3[ids[2]][0])
+			print('2. ' + Z3[ids[1]][0])
+			print('3. ' + Z3[ids[0]][0])
+			c = int(input('Pertanyaan ke-'))
+			
+			while (not(ok)):
+				if (c == 1):
+					idx = ids[2]
+					ok = True
+				elif (c == 2):
+					idx = ids[1]
+					ok = True
+				elif (c == 3):
+					idx = ids[0]
+					ok = True
+				else:
+					c = int(input('Pertanyaan ke-'))
 	else:
 		idx = -1
 
 	if (idx != -1):
 		print(Z3[idx][1])
 	else:
-		print("Saya tidak mengerti maksud Anda :(")
+		print('Saya tidak mengerti maksud Anda :(')
--- regex.py
+++ regex.py
@@ -1,29 +1,35 @@
 import re
 
 def regex(T, P):
-	stext = P.split()
-	lengthP = len(stext)
+	text = P.split()
+	lengthP = len(text)
 	lengthT = len(T)
+	percent = [0 for i in range(lengthT)]
 	found = False
 	i = 0
 
 	while ((i < lengthT) and not(found)):
 		j = 0
-		rtext = ''
+		length = 0
 		valid = True
 		
 		while ((j < lengthP) and (valid)):
-			rtext += stext[j]
-			
-			if (j != (len(stext) - 1)):
-				rtext += ' '
-			
-			x = re.search(rtext, T[i][0])
+			x = re.search(text[j], T[i][0])
 			
 			if (x == None):
 				valid = False
 			else:
+				length += len(text[j])
+				
+				if (j != (lengthP - 1)):
+					length += 1
+				
 				j += 1
+
+		if ((len(P) > len(T[i][0])) and (length != 0)):
+			percent[i] = float(len(T[i][0])/len(P))
+		else:
+			percent[i] = float(length/len(T[i][0]))
 		
 		if (j == lengthP):
 			found = True
@@ -31,6 +37,6 @@ def regex(T, P):
 			i += 1
 	
 	if (found):
-		return i
+		return i, percent
 	else:
 		return -1
