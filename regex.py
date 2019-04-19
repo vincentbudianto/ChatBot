e3e0c0d4bbdbe5b76389ff200270940fbfbe4798
vincent VB <vincent.budianto@yahoo.co.id>
2019-04-19 17:27:44 +0700

    improved regex function

--- regex.py
+++ regex.py
@@ -6,25 +6,20 @@ def regex(T, P):
 	lengthR = len(P)
 	lengthT = len(T)
 	percent = [0 for i in range(lengthT)]
-	found = False
 	i = 0
 	
-	#while ((i < lengthT) and not(found)):
-	while (i < 30):
+	while (i < lengthT):
 		j = 0
 		length = 0
 		textQ = T[i][0].split()
 		lengthQ = len(textQ)
 		lengthS = len(T[i][0])
-		valid = True
 		
 		if (lengthP > lengthQ):
-			while ((j < lengthQ) and valid):
+			while (j < lengthQ):
 				x = re.search(textQ[j], P)
 				
-				if (x == None):
-					valid = False
-				else:
+				if (x != None):
 					length += len(textQ[j])
 					
 					if (j != (lengthQ - 1)):
@@ -34,28 +29,19 @@ def regex(T, P):
 			
 			percent[i] = float(length/lengthR)
 		else:
-			while ((j < lengthP) and valid):
+			while (j < lengthP):
 				x = re.search(textP[j], T[i][0])
 				
-				if (x == None):
-					valid = False
-				else:
+				if (x != None):
 					length += len(textQ[j])
 					
 					if (j != (lengthP - 1)):
 						length += 1
-				
+					
 				j += 1
 			
 			percent[i] = float(length/lengthS)
-		
-		#if (((j == lengthP) or (j == lengthQ)) and valid):
-		#	found = True
-		#else:
-			#i += 1
+	
 		i += 1
 	
-	if (found):
-		return [i, percent]
-	else:
-		return [-1, percent]
+	return (i, percent)
