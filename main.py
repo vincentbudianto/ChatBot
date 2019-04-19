1edbb20b5c3b719e4310e768adcdf9d5e67373bd
vincent VB <vincent.budianto@yahoo.co.id>
2019-04-19 09:48:41 +0700

    fixed regex

--- main.py
+++ main.py
@@ -6,99 +6,141 @@ from regex import *
 from synonim import *
 
 def readFile(str):
-    f = open(str, encoding="utf8")
-    contents = f.read()
-    return contents
+	f = open(str, encoding = "utf8")
+	contents = f.read()
+	return contents
 
 def getanswer(id):
 	idx = 0
+	
 	for i in range(id):
 		if (Q[i] == '\n'):
-			idx = idx+1
+			idx = idx + 1
 
 	str = ''
-	while (Q[id] != '\n'):
-		id = id-1
 	
-	while (Q[id+1] != '\n'):
-		str += Q[id+1]
-		id = id+1
+	while (Q[id] != '\n'):
+		id = id - 1
+	
+	while (Q[id + 1] != '\n'):
+		str += Q[id + 1]
+		id = id + 1
 
 	print(Z3[idx][1])
 
 if __name__ == "__main__":
-    factory = StopWordRemoverFactory()
-    stopword = factory.create_stop_word_remover()
+	factory = StopWordRemoverFactory()
+	stopword = factory.create_stop_word_remover()
 
-    T = readFile("pertanyaan.txt")
-    Z1 = T.split('\n')
-    Z2 = []
-    Z3 = []
+	T = readFile("pertanyaan.txt")
+	Z1 = T.split('\n')
+	Z2 = []
+	Z3 = []
+	Q = ''
+	
+	for i in range(len(Z1)):
+		Z2.append(Z1[i].split('. '))
 
-    for i in range(len(Z1)):
-        Z2.append(Z1[i].split('. '))
+	for j in range(len(Z2)):
+		Z3.append(Z2[j][1].split('? '))
 
-    for j in range(len(Z2)):
-        Z3.append(Z2[j][1].split('? '))
+	for k in range(len(Z3)):
+		Z3[k][0] = Z3[k][0].replace('-', ' ')
+		Z3[k][0] = stopword.remove(changeSinonim(Z3[k][0])).lower()
+		temp = changeSinonim(Z3[k][0])
+		kata = len(temp.split(' '))
+		
+		for i in range(kata):
+			temp = stopword.remove(temp)
+		
+		Q += temp
+		
+		if (k != len(Z3)):
+			Q += '\n'
 
-    Q = ''	
-    for k in range(len(Z3)):
-        temp = changeSinonim(Z3[k][0])
-        kata = len(temp.split(' '))
-        for i in range(kata):
-            temp = stopword.remove(temp)
-        Q += temp
-        
-        if (k != len(Z3)):
-            Q += '\n'
+	P = sys.argv[1]
+	P = P.replace('?', '')
+	P = P.replace('-', ' ')
+	word = changeSinonim(P)
+	kata = len(word.split(' '))
 
-    P = sys.argv[1]
-    P = P.replace('?','')
-    word = changeSinonim(P)
-    kata = len(word.split(' '))
-    for i in range(kata):
-        word = stopword.remove(word)
+	for i in range(kata):
+		word = stopword.remove(word).lower()
 
-    pil = int(sys.argv[2])
-    if (pil == 1):
-        id = Boyer_Moore(Q,word)
-    elif (pil == 2):
-        id = KMP(Q,word)
+	pil = int(sys.argv[2])
 
-    idx = 0
-    if (id != -1):
-        getanswer(id)
-    else:
-        id = regex(Z3, word)[0]
-        percent = regex(Z3, word)[1]
-        print(percent)
-        if (id != -1):
-            getanswer(id)
-        else:
-            idr = sorted(range(len(percent)), key=lambda i: percent[i])[-3:]
-            ids = []
-            
-            for m in range(3):
-                if (percent[idr[m]] != 0):
-                    ids.insert(0, idr[m])
-            
-            print(ids)
-            if (len(ids) >= 1):
-                if (percent[ids[0]] >= 0.9):
-                    print(ids[0])
-                    print(Z3[ids[0]][1])
-                else:
-                    if (len(ids) == 1):
-                        print('Mungkin maksud anda adalah:\n')
-                        print('1. ' + Z3[ids[0]][0])
-                    elif (len(ids) == 2):
-                        print('Mungkin maksud anda adalah:\n')
-                        print('1. ' + Z3[ids[0]][0])
-                        print('2. ' + Z3[ids[1]][0])
-                    elif (len(ids) == 3):
-                        print('Mungkin maksud anda adalah:\n')
-                        print('1. ' + Z3[ids[0]][0])
-                        print('2. ' + Z3[ids[1]][0])
-                        print('3. ' + Z3[ids[2]][0])
-            else:
-                print("Maaf saya tidak mengerti maksud Anda :(")			
+	if (pil == 1):
+		id = Boyer_Moore(Q,word)
+	elif (pil == 2):
+		id = KMP(Q,word)
+	
+	idx = 0
+
+	if (id != -1):
+		getanswer(id)
+	else:
+		id, percent = regex(Z3, word)
+		
+		if (id != -1):
+			print(Z3[id][1])
+		else:
+			idr = sorted(range(len(percent)), key=lambda i: percent[i])[-3:]
+			ids = []
+			
+			for m in range(3):
+				if (percent[idr[m]] != 0):
+					ids.insert(0, idr[m])
+			
+			if (len(ids) >= 1):
+				if (percent[ids[0]] >= 0.9):
+					print(Z3[ids[0]][1])
+				else:
+					ok = False
+			
+					if (len(ids) == 1):
+						print('Mungkin maksud anda adalah:\n')
+						print('1. ' + Z3[ids[0]][0])
+						c = int(input('Pertanyaan ke-'))
+						
+						while (not(ok)):
+							if (c == 1):
+								idx = ids[0]
+								ok = True
+					elif (len(ids) == 2):
+						print('Mungkin maksud anda adalah:')
+						print('1. ' + Z3[ids[0]][0])
+						print('2. ' + Z3[ids[1]][0])
+						c = int(input('Pertanyaan ke-'))
+						
+						while (not(ok)):
+							if (c == 1):
+								idx = ids[0]
+								ok = True
+							elif (c == 2):
+								idx = ids[1]
+								ok = True
+							else:
+								c = int(input('Pertanyaan ke-'))
+					elif (len(ids) == 3):
+						print('Mungkin maksud anda adalah:')
+						print('1. ' + Z3[ids[0]][0])
+						print('2. ' + Z3[ids[1]][0])
+						print('3. ' + Z3[ids[2]][0])
+						c = int(input('Pertanyaan ke-'))
+						
+						while (not(ok)):
+							if (c == 1):
+								idx = ids[0]
+								ok = True
+							elif (c == 2):
+								idx = ids[1]
+								ok = True
+							elif (c == 3):
+								idx = ids[2]
+								ok = True
+							else:
+								c = int(input('Pertanyaan ke-'))
+					
+					print(Z3[idx][1])
+			else:
+				print("Maaf saya tidak mengerti maksud Anda :(")			
--- regex.py
+++ regex.py
@@ -1,40 +1,59 @@
 import re
 
 def regex(T, P):
-	text = P.split()
-	lengthP = len(text)
+	textP = P.split()
+	lengthP = len(textP)
+	lengthR = len(P)
 	lengthT = len(T)
 	percent = [0 for i in range(lengthT)]
 	found = False
 	i = 0
-
-	while ((i < lengthT) and not(found)):
+	
+	#while ((i < lengthT) and not(found)):
+	while (i < 30):
 		j = 0
 		length = 0
+		textQ = T[i][0].split()
+		lengthQ = len(textQ)
+		lengthS = len(T[i][0])
 		valid = True
 		
-		while ((j < lengthP) and (valid)):
-			x = re.search(text[j], T[i][0])
-			
-			if (x == None):
-				valid = False
-			else:
-				length += len(text[j])
+		if (lengthP > lengthQ):
+			while ((j < lengthQ) and valid):
+				x = re.search(textQ[j], P)
 				
-				if (j != (lengthP - 1)):
-					length += 1
+				if (x == None):
+					valid = False
+				else:
+					length += len(textQ[j])
+					
+					if (j != (lengthQ - 1)):
+						length += 1
 				
 				j += 1
-
-		if ((len(P) > len(T[i][0])) and (length != 0)):
-			percent[i] = float(len(T[i][0])/len(P))
+			
+			percent[i] = float(length/lengthR)
 		else:
-			percent[i] = float(length/len(T[i][0]))
+			while ((j < lengthP) and valid):
+				x = re.search(textP[j], T[i][0])
+				
+				if (x == None):
+					valid = False
+				else:
+					length += len(textQ[j])
+					
+					if (j != (lengthP - 1)):
+						length += 1
+				
+				j += 1
+			
+			percent[i] = float(length/lengthS)
 		
-		if (j == lengthP):
-			found = True
-		else:
-			i += 1
+		#if (((j == lengthP) or (j == lengthQ)) and valid):
+		#	found = True
+		#else:
+			#i += 1
+		i += 1
 	
 	if (found):
 		return [i, percent]
